685556afaf2c9a96676617f420019a2e6687dfa2
warlordjones <warlordjones@gmail.com>
2019-07-27 16:27:18 +0100

    Delete now unnecessary object holder

--- src/main/java/com/minerarcana/floralchemy/FloraObjectHolder.java
+++
@@ -1,9 +0,0 @@
-package com.minerarcana.floralchemy;
-
-import net.minecraft.block.Block;
-import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
-
-@ObjectHolder(value = Floralchemy.MOD_ID)
-public class FloraObjectHolder {
-    public static final Block hedge = null;
-}
